d6a29a9304c2cbc1c9bc67f221fb72af673b5314
oliver <olivercastroteran@gmail.com>
2020-10-03 18:04:10 -0400

    fetching data from API pasing api key, language and movie as params

--- src/Components/UI/SearchMovies/SearchMovies.js
+++ src/Components/UI/SearchMovies/SearchMovies.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchMovies.scss';
 
 const SearchMovies = () => {
+  const [input, setInput] = useState('');
+  const [language, setLanguage] = useState('en');
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log(input);
+    const api_key = '0559217f931948d53686513322d626c7';
+    const query = input;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=${language}-US&query=${query}`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setInput('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="search">
-      <form className="search__form">
+      <form className="search__form" onSubmit={searchMovies}>
         <label htmlFor="query" className="search__label">
           Movie Name:
         </label>
@@ -14,7 +34,33 @@ const SearchMovies = () => {
           className="search__input"
           name="query"
           placeholder="i.e. Interstellar"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
+        <div className="search__language">
+          <button
+            className={
+              language === 'en'
+                ? 'search__language--opt search__language--opt-1 selected'
+                : 'search__language--opt search__language--opt-1'
+            }
+            type="button"
+            onClick={(e) => setLanguage('en')}
+          >
+            En
+          </button>
+          <button
+            className={
+              language === 'es'
+                ? 'search__language--opt search__language--opt-2 selected'
+                : 'search__language--opt search__language--opt-2'
+            }
+            type="button"
+            onClick={(e) => setLanguage('es')}
+          >
+            Es
+          </button>
+        </div>
         <button className="search__btn" type="submit">
           Search
         </button>
